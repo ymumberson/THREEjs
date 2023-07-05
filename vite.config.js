d8a7765a3431e8b4873f6c8d2e1e030347766e89
Yoshan Mumberson <mryosh@outlook.com>
2023-07-05 14:43:26 +0100

    Removed old deploys

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,17 @@
 // vite.config.js
-export default {
-    // config options
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/THREEjs/'
+  }
+
+  return config
+})
