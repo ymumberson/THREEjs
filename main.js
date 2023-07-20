2823438864e9c800f9ff98d77542532334834fd4
Yoshan Mumberson <mryosh@outlook.com>
2023-07-20 15:32:05 +0100

    Refactored physics engine

--- main.js
+++ main.js
@@ -4,118 +4,28 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import * as CANNON from "cannon";
 import { PhysicsScene } from './physicsScene.js';
 
-// // Scene
-// const scene = new THREE.Scene();
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+const FOV = 45;
+let my_scene = new PhysicsScene(sizes.width, sizes.height, FOV, sizes.width / sizes.height, [0, -20, 1]);
+my_scene.AddSphere(1, 0x00ff83, 10, [0,0,10]);
+my_scene.AddSphere(3, 0xffff83, 5, [6,0,10]);
+my_scene.AddPlane();
 
-// // Create the sphere
-// const RADIUS = 3;
-// const SEGMENTS = 64;
-// const geometry = new THREE.SphereGeometry(RADIUS, SEGMENTS, SEGMENTS);
-// const material = new THREE.MeshStandardMaterial({
-//     color: 0x00ff83,
-//     roughness: 0.5,
-// });
-// const mesh = new THREE.Mesh(geometry, material);
-// scene.add(mesh);
+await new Promise(r => setTimeout(r, 5000));
+for (var i=0; i<100; ++i) {
+    my_scene.AddSphere(1, 0x00ff83, 10, [Math.random()*10,Math.random()*10,20]);
+    // await new Promise(r => setTimeout(r, 100));
+}
 
-// // Light
-// const light = new THREE.PointLight(0xffffff, 1, 100);
-// light.position.set(0, 10, 10);
-// light.intensity = 1.25;
-// scene.add(light);
-
-// // Camera
 // const sizes = {
 //     width: window.innerWidth,
 //     height: window.innerHeight
 // }
 // const FOV = 45;
-// const camera = new THREE.PerspectiveCamera(FOV, sizes.width / sizes.height);
-// camera.position.z = 20;
-// scene.add(camera);
-
-// // Renderer
-// const canvas = document.querySelector(".webgl");
-// const renderer = new THREE.WebGLRenderer({
-//     canvas: canvas
-// });
-// renderer.setSize(sizes.width, sizes.height);
-// renderer.setPixelRatio(2);
-// renderer.render(scene, camera);
-
-// // Controls
-// const controls = new OrbitControls(camera, canvas);
-// controls.enableDamping = true;
-// controls.enablePan = false;
-// controls.enableZoom = false;
-// // controls.autoRotate = true;
-// // controls.autoRotateSpeed = 5;
-
-// // Resize
-// window.addEventListener('resize', () => {
-//     // Update Sizes
-//     sizes.width = window.innerWidth;
-//     sizes.height = window.innerHeight;
-
-//     // Update Camera
-//     camera.aspect = sizes.width / sizes.height;
-//     camera.updateProjectionMatrix();
-//     renderer.setSize(sizes.width, sizes.height);
-//     // renderer.render(scene, camera);
-// })
-
-// const loop = () => {
-//     controls.update();
-//     renderer.render(scene, camera);
-//     window.requestAnimationFrame(loop);
-// }
-// loop();
-
-
-// /* Cannon.js stuff ie Setting up the physics world*/
-// // World setup
-// var world = new CANNON.World();
-// world.gravity.set(0,0,-9.82); // m/s^2
-
-// // Create a sphere
-// var radius = 1; // m
-// var sphereBody = new CANNON.Body({
-//     mass: 5, //kg
-//     position: new CANNON.Vec3(0,0,10), // m
-//     shape: new CANNON.Sphere(radius)
-// });
-// world.addBody(sphereBody);
-
-// // Create a plane
-// var groundBody = new CANNON.Body({
-//     mass: 0, // Mass == 0 makes the body static
-// })
-// var groundShape = new CANNON.Plane();
-// groundBody.addShape(groundShape);
-// world.addBody(groundBody);
-
-// var fixedTimeStep = 1.0 / 60.0; // seconds
-// var maxSubSteps = 3;
-
-// // Start the simulation loop
-// var lastTime;
-// (function simloop(time) {
-//     requestAnimationFrame(simloop);
-//     if (lastTime != undefined) {
-//         var dt = (time - lastTime) / 1000;
-//         world.step(fixedTimeStep, dt, maxSubSteps);
-//     }
-//     // console.log("Sphere z position: " + sphereBody.position.z);
-//     lastTime = time;
-
-//     mesh.position.z = sphereBody.position.z;
-// })();
-
-let my_scene = new PhysicsScene();
-
-// const loop = () => {
-//     // controls.update();
-//     // this.renderer.render(this.scene, this.camera);
-//     // window.requestAnimationFrame(loop);
-// }
-// loop();
+// this.camera = new THREE.PerspectiveCamera(FOV, sizes.width / sizes.height);
+// this.camera.position.y = -20;
+// this.camera.position.z = 1;
+// this.scene.add(this.camera);
--- physicsScene.js
+++ physicsScene.js
@@ -4,28 +4,24 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import * as CANNON from "cannon";
 
 class PhysicsScene {
-    constructor() {
+    constructor(canvas_width, canvas_height, fov, aspect_ratio, camera_position) {
         this.scene = new THREE.Scene();
         this.world = new CANNON.World();
         this.world.gravity.set(0,0,-9.82); // m/s^2
-
-        this.AddSphere(3, 0x00ff83, 5, [0,0,0]);
-        this.AddSphere(3, 0xffff83, 5, [6,0,0]);
+        this.physics_world = [];
+        this.render_world = [];
 
         // Light
         const light = new THREE.PointLight(0xffffff, 1, 100);
-        light.position.set(0, 10, 10);
+        light.position.set(0, -10, 10);
         light.intensity = 1.25;
         this.scene.add(light);
 
         // Camera
-        const sizes = {
-            width: window.innerWidth,
-            height: window.innerHeight
-        }
-        const FOV = 45;
-        this.camera = new THREE.PerspectiveCamera(FOV, sizes.width / sizes.height);
-        this.camera.position.z = 20;
+        this.camera = new THREE.PerspectiveCamera(fov, aspect_ratio);
+        this.camera.position.x = camera_position[0];
+        this.camera.position.y = camera_position[1];
+        this.camera.position.z = camera_position[2];
         this.scene.add(this.camera);
 
         // Renderer
@@ -33,65 +29,83 @@ class PhysicsScene {
         this.renderer = new THREE.WebGLRenderer({
             canvas: canvas
         });
-        this.renderer.setSize(sizes.width, sizes.height);
+        this.renderer.setSize(canvas_width, canvas_height);
         this.renderer.setPixelRatio(2);
         this.renderer.render(this.scene, this.camera);
+
+        this.controls = new OrbitControls(this.camera, canvas);
+        this.controls.enableDamping = true;
 
         this.deltaTime = 1 / 60; // Time step for physics simulation (1/60 sec)
         this.maxSubSteps = 3; // Maximum number of substeps to perform per frame
         this.lastTime = 0; // To keep track of the elapsed time between frames
 
-        this.simulationLoop = this.simulationLoop.bind(this);
-        this.simulationLoop();
+        this.SimulationLoop = this.SimulationLoop.bind(this);
+        this.SimulationLoop();
 
         console.log('Created new physics scene!');
     }
     
-    simulationLoop() {
+    SimulationLoop() {
         const currentTime = performance.now();
         const elapsedMilliseconds = currentTime - this.lastTime;
         this.lastTime = currentTime;
     
+        // Update the physics world
         this.world.step(this.deltaTime, elapsedMilliseconds / 1000, this.maxSubSteps);
     
         // Update the objects in your scene based on the updated physics simulation
-        // this.my_mesh.position.z = this.my_mesh_physics.position.z;
-        let physics_world = this.world.bodies;
-        let render_world = this.scene.children;
-        for (var i=0; i<physics_world.length; ++i) {
-            render_world[i].position.x = physics_world[i].position.x;
-            render_world[i].position.y = physics_world[i].position.y;
-            render_world[i].position.z = physics_world[i].position.z;
+        for (var i=0; i<this.physics_world.length; ++i) {
+            this.render_world[i].position.x = this.physics_world[i].position.x;
+            this.render_world[i].position.y = this.physics_world[i].position.y;
+            this.render_world[i].position.z = this.physics_world[i].position.z;
 
-            render_world[i].quaternion.x = physics_world[i].quaternion.x;
-            render_world[i].quaternion.y = physics_world[i].quaternion.y;
-            render_world[i].quaternion.z = physics_world[i].quaternion.z;
-            render_world[i].quaternion.w = physics_world[i].quaternion.w;
+            this.render_world[i].quaternion.x = this.physics_world[i].quaternion.x;
+            this.render_world[i].quaternion.y = this.physics_world[i].quaternion.y;
+            this.render_world[i].quaternion.z = this.physics_world[i].quaternion.z;
+            this.render_world[i].quaternion.w = this.physics_world[i].quaternion.w;
         }
 
         // Call the render function to draw the updated scene (if you are using Three.js or other rendering library)
+        this.controls.update();
         this.renderer.render(this.scene, this.camera);
-
-        requestAnimationFrame(this.simulationLoop);
-      }
+        
+        // Request animation call for next frame
+        requestAnimationFrame(this.SimulationLoop);
+    }
 
     AddSphere(radius, colour, mass, position) {
         const SEGMENTS = 64;
         const geometry = new THREE.SphereGeometry(radius, SEGMENTS, SEGMENTS);
         const material = new THREE.MeshStandardMaterial({
             color: colour,
-            // roughness: 0.5,
+            roughness: 0.5,
         });
         const mesh = new THREE.Mesh(geometry, material);
         this.scene.add(mesh);
+        this.render_world.push(mesh);
 
         var sphereBody = new CANNON.Body({
             mass: mass, //kg
-            // position: new CANNON.Vec3(0,0,10), // m
             position: new CANNON.Vec3(position[0],position[1],position[2]), // m
             shape: new CANNON.Sphere(radius)
         });
         this.world.addBody(sphereBody);
+        this.physics_world.push(sphereBody);
+    }
+
+    AddPlane() {
+        const geometry = new THREE.PlaneGeometry( 10000 , 10000 );
+        const material = new THREE.MeshBasicMaterial( {color: 0x009A17, side: THREE.DoubleSide} );
+        const plane = new THREE.Mesh( geometry, material );
+        this.scene.add( plane ); 
+
+        var groundBody = new CANNON.Body({
+            mass: 0 // mass == 0 makes the body static
+        });
+        var groundShape = new CANNON.Plane();
+        groundBody.addShape(groundShape);
+        this.world.addBody(groundBody);
     }
 }
 
